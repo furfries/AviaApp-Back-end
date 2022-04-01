b305dd38b52d136ed3d58cdfc9e34758bc426f01
p.kryshyn <pavel.krishin@gmail.com>
2022-04-01 14:41:00 +0300

    Disable swagger

--- AviaApp/AviaApp/Startup.cs
+++ AviaApp/AviaApp/Startup.cs
@@ -69,42 +69,42 @@ namespace AviaApp
                     };  
                 }); 
 
-            services.AddSwaggerGen(swagger =>
-            {
-                //This is to generate the Default UI of Swagger Documentation    
-                swagger.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "AviaApp",
-                });
-                // To Enable authorization using Swagger (JWT)    
-                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description =
-                        "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
-                });
-                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] { }
-                    }
-                });
-                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                swagger.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-            });
+            // services.AddSwaggerGen(swagger =>
+            // {
+            //     //This is to generate the Default UI of Swagger Documentation    
+            //     swagger.SwaggerDoc("v1", new OpenApiInfo
+            //     {
+            //         Version = "v1",
+            //         Title = "AviaApp",
+            //     });
+            //     // To Enable authorization using Swagger (JWT)    
+            //     swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            //     {
+            //         Name = "Authorization",
+            //         Type = SecuritySchemeType.ApiKey,
+            //         Scheme = "Bearer",
+            //         BearerFormat = "JWT",
+            //         In = ParameterLocation.Header,
+            //         Description =
+            //             "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
+            //     });
+            //     swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+            //     {
+            //         {
+            //             new OpenApiSecurityScheme
+            //             {
+            //                 Reference = new OpenApiReference
+            //                 {
+            //                     Type = ReferenceType.SecurityScheme,
+            //                     Id = "Bearer"
+            //                 }
+            //             },
+            //             new string[] { }
+            //         }
+            //     });
+            //     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //     swagger.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            // });
 
             services.AddCors(options =>
             {
@@ -127,8 +127,8 @@ namespace AviaApp
                 app.UseDeveloperExceptionPage();
             }
             
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AviaApp v1"));
+            // app.UseSwagger();
+            // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AviaApp v1"));
 
             app.UseHttpsRedirection();
 
