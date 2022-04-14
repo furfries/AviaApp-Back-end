d707bed9a30f06d40040803e60e6038da8003755
p.kryshyn <pavel.krishin@gmail.com>
2022-04-14 14:49:09 +0300

    14 - Implement quick search by locations

---
+++ AviaApp/AviaApp/Controllers/QuickSearchController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AviaApp.Models.ViewModels;
+using AviaApp.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AviaApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class QuickSearchController : ControllerBase
+{
+    private readonly IQuickSearchService _quickSearchService;
+
+    public QuickSearchController(IQuickSearchService quickSearchService)
+    {
+        _quickSearchService = quickSearchService;
+    }
+
+    /// <summary>
+    /// Returns list of locations by search string
+    /// </summary>
+    /// <remarks>
+    /// The endpoint returns the first five locations found
+    /// </remarks>
+    [HttpGet]
+    [Route("/location")]
+    [ProducesResponseType(typeof(List<LocationViewModel>), 200)]
+    public async Task<IActionResult> GetFlightsByDateRangeAsync(string searchString)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(searchString) || string.IsNullOrWhiteSpace(searchString))
+                return Ok(new List<LocationViewModel>());
+
+            return Ok(await _quickSearchService.GetLocationsAsync(searchString));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
---
+++ AviaApp/AviaApp/Services/QuickSearchService.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using AviaApp.Models.ViewModels;
+using AviaApp.Services.Contracts;
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AviaApp.Services;
+
+public class QuickSearchService : IQuickSearchService
+{
+    private readonly AviaAppDbContext _context;
+    private readonly IMapper _mapper;
+    private const int LocationsCount = 5;
+
+    public QuickSearchService(AviaAppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<IList<LocationViewModel>> GetLocationsAsync(string searchString)
+    {
+        var locations = new List<LocationViewModel>();
+
+        var airports = await _context.Airports
+            .Include(x => x.City.Country)
+            .Where(x => x.Name.Contains(searchString))
+            .OrderBy(x => x.Name)
+            .Take(LocationsCount)
+            .ToListAsync();
+
+        locations.AddRange(_mapper.Map<IList<LocationViewModel>>(airports));
+
+        if (locations.Count == LocationsCount)
+            return locations;
+
+        var cities = await _context.Cities
+            .Include(x => x.Country)
+            .Where(x => x.Name.Contains(searchString))
+            .OrderBy(x => x.Name)
+            .Take(LocationsCount - locations.Count)
+            .ToListAsync();
+
+        locations.AddRange(_mapper.Map<IList<LocationViewModel>>(cities));
+
+        if (locations.Count == LocationsCount)
+            return locations;
+
+        var countries = await _context.Countries
+            .Where(x => x.Name.Contains(searchString))
+            .OrderBy(x => x.Name)
+            .Take(LocationsCount - locations.Count)
+            .ToListAsync();
+
+        locations.AddRange(_mapper.Map<IList<LocationViewModel>>(countries));
+
+        return locations;
+    }
+}
